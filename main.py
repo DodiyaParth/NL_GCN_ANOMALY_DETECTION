da91475e09c2a867ce278d503c199c428917650b
DodiyaParth <parthdodiya999@gmail.com>
2021-03-08 12:46:41 +0530

    model N2 added

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from train import *
 
-for dataset in ['Disney','Amazon','facebook','Enron']:
-    train('N1',dataset,logging=False,epochs=150)
+def test1():
+    for dataset in ['Disney','Amazon','facebook','Enron']:
+        train('N1',dataset,logging=False,epochs=100)
+
+def test2():
+    for dataset in ['Disney','Amazon','facebook']:
+        train('N2',dataset,logging=False,epochs=80)
+
+test2()
