36cd9d2a547116d5fca9d7668b493790e13fa730
DodiyaParth <parthdodiya999@gmail.com>
2021-03-09 19:35:35 +0530

    D0.43 A0.6233 F0.6495 E0.73 160epochs

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def test1():
         train('N1',dataset,logging=False,epochs=100)
 
 def test2():
-    for dataset in ['Disney','Amazon','facebook']:
-        train('N2',dataset,logging=False,epochs=80)
+    for dataset in ['Disney','Amazon','facebook','Enron']:
+        train('N2',dataset,logging=False,epochs=160)
 
-# test2()
-train('N2','facebook',logging=False,epochs=100)
+test2()
+# train('N2','Enron',logging=False,epochs=10)
