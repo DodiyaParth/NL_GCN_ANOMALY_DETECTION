d4ac0ed937b33b2a6fffe74951925e280cf06782
DodiyaParth <parthdodiya999@gmail.com>
2021-03-13 16:03:43 +0530

    N2 model

--- models.py
+++ models.py
@@ -92,35 +92,43 @@ class Net2(torch.nn.Module):
         super(Net2, self).__init__()
         self.conv1 = GCNConv(A,nfeat, nhid1)
         self.conv2 = GCNConv(A,nhid1, nhid2)
-        # self.nlgcn=NLGCN(nhid2)
-        # self.dense1=nn.Linear(nhid2,nhid2)
-        # self.dense2=nn.Linear(nhid2,nhid2)
+        self.nlgcn=NLGCN(nhid2)
+        self.nlgcn1=NLGCN(nhid2)
+        self.nlgcn2=NLGCN(nhid2)
+        self.dense1=nn.Linear(nhid2,nhid2)
+        self.dense2=nn.Linear(nhid2,nhid2)
         self.conv3 = GCNConv(A,nhid2, nhid3)
-        self.conv4 = GCNConv(A,nhid3, nout)
-        # self.dec2 = GCNConv(A,nhid2,nhid3)
+        # self.conv3_1 = GCNConv(A,nhid3, nhid3)
+        # self.conv4 = GCNConv(A,nhid3, nout)
         self.dec2 = nn.Linear(nhid2,nhid3)
         
     def forward(self,X):
         H  = self.conv1(X)
         H = torch.relu(H)
+
         H  = self.conv2(H)
         H = torch.relu(H)
-        # H=self.nlgcn(H)
+
+        H=self.nlgcn(H)
+        H=torch.relu(H)
+        H=self.nlgcn(H)
+        H=torch.relu(H)
+        H=self.nlgcn(H)
 
         A=self.dec2(H)
         A=torch.matmul(H,H.T)
         A=torch.sigmoid(A)
         
-        # H = self.dense1(H)
-        # H = torch.relu(H)
-        # H = self.dense2(H)
-        # H = torch.relu(H)
+        H = self.dense1(H)
+        H = torch.relu(H)
+        H = self.dense2(H)
+        H = torch.relu(H)
         
-
-
         Att  = self.conv3(H)
-        Att = torch.relu(Att)
-        Att = self.conv4(Att)
+        # Att = torch.relu(Att)
+        # Att  = self.conv3_1(Att)
+        # Att = torch.relu(Att)
+        # Att = self.conv4(Att)
         Att = torch.softmax(Att,1)
         return Att,A
 
